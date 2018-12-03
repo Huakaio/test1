9f87301a1966b9efa879472e26e4a6901ffc306c
Huakaio <1924869795@qq.com>
2018-12-03 22:46:31 +0800

    添加了prepareSlideshow函数

--- scripts/global.js
+++ scripts/global.js
@@ -48,7 +48,7 @@ function highlightPage() {
 addLoadEvent(highlightPage);
 function moveElement(elementID,final_x,final_y,interval) {
     if(!document.getElementById) return false;
-    if(!document.getElementsById(elementID)) return false;
+    if(!document.getElementById(elementID)) return false;
     var elem=document.getElementById(elementID);
     if(elem.movement){
         clearTimeout(elem.movement);
@@ -57,34 +57,79 @@ function moveElement(elementID,final_x,final_y,interval) {
         elem.style.left="0px";
     }
     if(!elem.style.top){
-         ele.style.top="0px";
+         elem.style.top="0px";
     }
     var xpos=parseInt(elem.style.left);
     var ypos=parseInt(elem.style.top);
     if(xpos==final_x&&ypos==final_y){
-        return ture;
+        return true;
     }
     if(xpos<final_x){
-        var dist=Math.ceil((final-xpos)/10);
+        var dist=Math.ceil((final_x-xpos)/10);
         xpos=xpos+dist;
     }
     if (ypos<final_y){
         var dist=Math.ceil((final_y-ypos)/10);
-        ypos+=dist;
+        ypos=ypos+dist;
     }
     if(xpos>final_x){
         var dist=Math.ceil((xpos-final_x)/10);
-        xpos-=dist;
+        xpos=xpos-dist;
     }
     if(ypos>final_y){
         var dist=Math.ceil((ypos-final_y)/10);
-        ypos-=dist;
+        ypos=ypos-dist;
     }
     elem.style.left=xpos+"px";
     elem.style.top=ypos+"px";
     var repeat="moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
     elem.movement=setTimeout(repeat,interval);
 }
+function prepareSlideshow() {
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("intro")) return false;
+    var intro =document.getElementById("intro");
+    var slideshow=document.createElement("div");
+    slideshow.setAttribute("id","slideshow");
+    var frame=document.createElement("img");
+    frame.setAttribute("id","frame");
+    frame.setAttribute("src","images/frame.gif");
+    frame.setAttribute("alt"," ");
+    slideshow.appendChild(frame);
+    var  preview = document.createElement("img");
+    preview.setAttribute("id","preview");
+    preview.setAttribute("src","images/slideshow.gif");
+    preview.setAttribute("alt","a glimpse of what awaits you");
+    slideshow.appendChild(preview);
+    insertAfter(slideshow,intro);
+    var links = document.getElementsByTagName("a");
+    var destination;
+    for (var i=0;i<links.length;i++){
+        links[i].onmouseover=function () {
+            destination=this.getAttribute("href");
+            if(destination.indexOf("index.html") != -1){
+                moveElement("preview",0,0,5);
+            }
+            if(destination.indexOf("about.html") != -1){
+                moveElement("preview",-150,0,5);
+            }
+            if(destination.indexOf("photos.html")!=-1){
+                moveElement("preview",-300,0,5);
+            }
+            if(destination.indexOf("live.html")!=-1){
+                moveElement("preview",-450,0,5);
+            }
+            if(destination.indexOf("contact.html")!=-1){
+                moveElement("preview",-600,0,5);
+            }
+        }
+    }
+}
+addLoadEvent(prepareSlideshow);
+
+
+
 
 
 
