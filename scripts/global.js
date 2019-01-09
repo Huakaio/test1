78524e9518e81d51c43c3878a3a7586a92a6dfb4
wangzhenghua <1924869795@qq.com>
2019-01-09 19:59:16 +0800

    function focusLabels

--- scripts/global.js
+++ scripts/global.js
@@ -315,10 +315,38 @@ function prepareForms() {
     for(var i=0;i<document.forms.length;i++){
         var thisform=document.forms[i];
         resetFields(thisform);
+        thisform.onsubmit=function () {
+            return validateForm(this);
+        }
     }
 }
 addLoadEvent(prepareForms);
-
+function isFilled(field) {
+    if(field.value.replace(' ','').length==0) return false;
+    var placeholder=field.placeholder|| field.getAttribute("placeholder");
+    return(field.value!=placeholder);
+}
+function isEmail(field) {
+    return(field.value.indexOf("@")!=-1&&field.value.indexOf(".")!=-1);
+}
+function validateForm(whichForm){
+    for(var i=0;i<whichForm.elements.length;i++){
+        var element=whichForm.elements[i];
+        if(element.required=='required'){
+            if(!isFilled(element)){
+                alert("Please fill in the"+element.name+"field.");
+                return false;
+            }
+        }
+        if(element.type=='email'){
+            if(!isEmail(element)){
+                alert("The "+element.name+" field must be a valid email address.");
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 
 
